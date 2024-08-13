b20ccf6d90d267594872c168779c975c148c1f72
cherneckiy <deneff.max@yandex.ru>
2024-08-13 23:05:09 +0300

    Добавьте файлы проекта.

---
+++ name_eman/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace name_eman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите свое имя");
+
+            var name =  Console.ReadLine();
+
+            for (int i = name.Length- 1; i >= 0; i--)
+            {
+                Console.WriteLine(name[i] + " ");
+            }
+        }
+    }
+}
